6fd70853979f079419f64d0d7232e35eb0b464a0
OrlPolinka <153023767+OrlPolinka@users.noreply.github.com>
2024-10-22 10:50:10 +0300

    Add files via upload

---
+++ lab5/lab5/lab5.cpp
@@ -0,0 +1,144 @@
+﻿#include <iostream>
+#include <vector>
+#include <climits>
+#include <chrono>
+using namespace std;
+
+int graph[8][8] = {
+	{0, 2, INT_MAX, 8, 2, INT_MAX, INT_MAX, INT_MAX},
+	{2, 0, 3, 10, 5, INT_MAX, INT_MAX, INT_MAX},
+	{INT_MAX, 3, 0, INT_MAX, 12, INT_MAX, INT_MAX, 7},
+	{8, 10, INT_MAX, 0, 14, 3, 1, INT_MAX},
+	{2, 5, 12, 14, 0, 11, 4, 8},
+	{INT_MAX, INT_MAX, INT_MAX, 3, 11, 0, 6, INT_MAX},
+	{INT_MAX, INT_MAX, INT_MAX, 1, 4, 6, 0, 9},
+	{INT_MAX, INT_MAX, 7, INT_MAX, 8, INT_MAX, 9, 0}
+};
+
+void algPrima(int start) {
+	int arr[7];
+	int current = 0;
+	vector<bool> visited(8, false);
+	vector<pair<int, int>> edg;
+	int weight = 0;
+	
+	auto start1 = chrono::steady_clock::now();
+
+	visited[start] = true;
+	while (current < 7) {
+
+		int x = -1, y = -1;
+		int min = INT_MAX;
+
+		for (int i = 0; i < 8; i++) {
+			if (visited[i]) {
+				for (int j = 0; j < 8; j++) {
+					if (!visited[j]) {
+						if (graph[i][j] < min && graph != 0) {
+							min = graph[i][j];
+							x = i;
+							y = j;
+						}
+					}
+				}
+			}
+		}
+
+		if (x != -1 && y != -1) {
+			visited[y] = true;
+
+			edg.push_back({ x, y });
+			arr[current] = min;
+			weight += min;
+
+			//cout << "Текущий вес графа: " << weight << endl;
+		}
+		current++;
+	}
+
+	for (int i = 0; i < edg.size(); i++) {
+		cout << "(" << edg[i].first + 1 << ", " << edg[i].second + 1 << ")     Вес ребра: " << arr[i] << endl;
+	}
+	cout << "\nВес остовного дерева: " << weight << endl;
+
+	auto end = chrono::steady_clock::now();
+	auto duration = chrono::duration_cast<chrono::microseconds>(end - start1).count();
+	cout << "Алгоритм Прима выполнен за " << duration << " микросекунд" << endl;
+
+	cout << "О-большое: " << pow(8, 2) << endl;
+	int V = 8;
+	int E = 16;
+	int y = E - V + 1;
+	cout << "Цикломатическое число: " << y << endl;
+}
+
+void algKraskala() {
+	int current = 0;
+	int arr[7];
+	vector<bool> visited(8, false);
+	vector<pair<int, int>> edg;
+	int weight = 0;
+
+	auto start1 = chrono::steady_clock::now();
+
+	visited[0] = true;
+
+	while (current < 7) {
+		int min = INT_MAX;
+		int x = -1, y = -1;
+
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if (!visited[j] && graph[i][j] != 0 && graph[i][j] != INT_MAX) {
+					if (graph[i][j] < min) {
+						min = graph[i][j];
+						x = i;
+						y = j;
+					}
+				}
+			}
+		}
+		if (x != -1 && y != -1) {
+			
+			visited[y] = true;
+			visited[x] = true;
+			edg.push_back({ x, y });
+			arr[current] = min;
+			weight += min;
+			
+		}
+		current++;
+	}
+
+	for (int i = 0; i < edg.size(); i++) {
+		cout << "(" << edg[i].first + 1 << ", " << edg[i].second + 1 << ")     Вес ребра: " << arr[i] << endl;
+	}
+	cout << "\nВес остовного дерева: " << weight << endl;
+
+	auto end = chrono::steady_clock::now();
+	auto duration = chrono::duration_cast<chrono::microseconds>(end - start1).count();
+	cout << "Алгоритм Краскала выполнен за " << duration << " микросекунд" << endl;
+
+	cout << "О-большое: " << current * log(current) << endl;
+	int V = 8;
+	int E = 16;
+	int y = E - V + 1;
+	cout << "Цикломатическое число: " << y << endl;
+}
+
+int main() {
+	setlocale(LC_ALL, "ru");
+	int start;
+	cout << "Введите стартовую вершину: ";
+	cin >> start;
+	while (start > 8 || start < 1) {
+		cout << "Ошибка! Неверный ввод! Повторите попытку: ";
+		cin >> start;
+	}
+	cout << "\nАлгоритм Прима\nСписок ребер:" << endl;
+	algPrima(start - 1);
+	cout << "\n\nАлгоритм Краскала\nСписок ребер:" << endl;
+	algKraskala();
+
+	return 0;
+}
---
+++ lab6/lab6/lab6.cpp
@@ -0,0 +1,153 @@
+﻿#include <iostream>
+#include <map>
+//#include <unordered_map>
+#include <string>
+#include <Windows.h>
+#include <queue>
+using namespace std;
+
+int global_id = 0;
+
+struct Node {
+    char ch;
+    int mp;
+    int id;
+    Node* left, *right;
+};
+
+Node* GetNode(char ch, int mp, Node* left, Node* right) {
+    Node* node = new Node();
+    node->ch = ch;
+    node->mp = mp;
+    node->id = global_id++;
+    node->left = left;
+    node->right = right;
+    return node;
+}
+
+//для сравнения узлов в очереди, чтоб выстроить их в определенном приоритете
+struct priority {
+    bool operator()(Node* l, Node* r) {
+        if (l->mp == r->mp) {
+            return l->id > r->id;
+        }
+        return l->mp > r->mp;
+    }
+};
+
+void encode(Node* root, string str, map<char, string>& code) {
+    if (root == NULL)
+        return;
+
+    if (!root->left && !root->right)
+        code[root->ch] = str;
+
+    encode(root->left, str + "1", code);
+    encode(root->right, str + "0", code);
+}
+
+void decode(Node* root, int& index, string str) {
+    if (root == NULL)
+        return;
+
+    if (!root->left && !root->right){
+        if (root->ch >= 'А' && root->ch <= 'Я' || root->ch >= 'A' && root->ch <= 'Z')
+            cout << " " << root->ch;
+        else
+            cout << root->ch;
+        return;
+    }
+
+    index++;
+
+    if (str[index] == '0') 
+        decode(root->right, index, str);
+    else
+        decode(root->left, index, str);
+
+}
+
+
+
+
+void algHafmana(string text) {
+
+    SetConsoleOutputCP(1251);
+
+    map<char, int> mp;  //для хранения частоты для каждого символа
+
+    for (char ch : text) {
+        mp[ch]++;
+    }
+
+    priority_queue<Node*, vector <Node*>, priority> q;  //приоритетная очередь
+
+    for (auto k : mp) {
+        q.push(GetNode(k.first, k.second, NULL, NULL));
+    }
+
+    cout << "Частота символов:" << endl;
+    for (auto k : mp) {
+        cout << k.first << " : " << k.second << endl;
+    }
+
+    while (q.size() != 1) {
+        Node* left = q.top();
+        q.pop();
+
+        Node* right = q.top();
+        q.pop();
+
+        int sum = left->mp + right->mp;
+        q.push(GetNode('\0', sum, left, right));
+    }
+
+    Node* root = q.top(); //корень дерева
+
+    map<char, string> code; //для хранения кодов символов
+
+    encode(root, "", code);
+    cout << "Коды символов:" << endl;
+    cout << "Символ:   " << "Код:" << endl;
+    for (auto k : code) {
+        cout << k.first << " : " << k.second << endl;
+    }
+
+    string str;
+    for (char ch : text) {
+        str += code[ch];
+        //str += " ";
+    }
+
+    cout << "Закодированная строка: " << str << endl;
+
+    int index = -1;
+
+    cout << "Декодированная строка: " << endl;
+    while (index < (int)str.size() - 2) {
+        decode(root, index, str);
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    string text;
+    cout << "Введите текст: ";
+    getline(cin, text);
+
+    string filteredText;
+    for (char ch : text) {
+        if (ch != ' ') {
+            filteredText += ch;
+        }
+
+    }
+
+    algHafmana(filteredText);
+
+    return 0;
+}
